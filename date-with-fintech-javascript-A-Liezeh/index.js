5a8cdb257347d9e24d424c487ba1af0dc5af89c4
A-Liezeh <127749628+A-Liezeh@users.noreply.github.com>
2023-05-07 08:55:18 +0300

    Add files via upload

---
+++ date-with-fintech-javascript-A-Liezeh/index.js
@@ -0,0 +1,45 @@
+import * as fs from 'node:fs';
+
+let DAYS = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+
+let dataRead = (err, data) => {
+  if (err) {
+      console.error(err);
+      return;
+  }
+  const dates = JSON.parse(data);
+  // console.log(dates);
+  console.log(formatDate(dates));
+}
+
+fs.readFile('data.json', 'utf8', dataRead);
+
+let formatDate = (data) => {
+  for(const place of data){
+    for(const item of place.events){
+      let currentDate=item.date.split("-")
+      let newDate={
+        year: currentDate[0],
+        month: currentDate[1],
+        day: currentDate[2]
+        };
+        item.date=newDate;
+    }
+  }
+}
+
+
+// DON'T MODIFY THE CODE BELOW THIS LINE
+
+let toExport;
+
+try {
+  toExport = [
+    { name: "holidaysByYear", content: holidaysByYear, type: "function" }
+  ]
+
+} catch (error) {
+  toExport = { error: error.message }
+}
+
+export { toExport };
